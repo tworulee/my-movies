faf5962c94655a54edcdc58497f09abcd2fd3f9d
Tuğrul Yüce <tugrulyuce80@gmail.com>
2023-12-06 15:56:43 +0300

    start+list-item

---
+++ src/components/Movies.js
@@ -0,0 +1,28 @@
+
+const Movies = (props) => {
+  return (
+    <div className="row">
+      {props.movies.map((movie) => (
+        <div className="col-lg-4" key={movie.id}>
+          <div className="card mb-4 shadow-sm">
+            <img src={movie.imageURL} className="card-img-top" alt="sample" />
+            <div className="card-body">
+              <h5 className="card-title">{movie.name}</h5>
+              <p className="card-text">{movie.overview}</p>
+              <div className="d-flex justify-content-between align-items-center">
+                <button type="button" className="btn btn-md btn-outline-danger">
+                  delete
+                </button>
+                <h2>
+                  <span className="badge bg-info">{movie.rating}</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Movies;
---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+
+class SearchBar extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>SearchBar</h1>
+        );
+    }
+}
+ 
+export default SearchBar;
